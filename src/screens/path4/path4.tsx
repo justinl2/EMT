79fedc71cfa5e099e47b04134d5f611ade629fd1
Cullen Ye <59585764+CullenLYe@users.noreply.github.com>
2023-10-02 19:46:59 -0400

    Added "Type Anything" section

--- src/screens/path4/path4.tsx
+++ src/screens/path4/path4.tsx
@@ -1,11 +1,71 @@
-import { Text } from "react-native";
+import React from 'react';
+import {
+    StyleSheet, Button, Text, View,
+    TextInput, TouchableWithoutFeedback, Keyboard, KeyboardAvoidingView
+} from "react-native";
 
+const TypeAnything = ({ navigation }) => {
 
-const Screen4 = () => {
+    const [text, onChangeText] = React.useState('')
 
-    return(
-        <Text>Screen 4</Text>
+    return (
+        <KeyboardAvoidingView behavior={Platform.OS === 'ios' ? 'padding' : 'height'} style={styles.container}>
+            <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+                <View style={styles.container}>
+
+                    {/* Text entry field */}
+                    <View style={styles.textEntry}>
+                        <TextInput
+                            multiline
+                            editable
+                            style={{ padding: 15, fontSize: 30 }}
+                            ref={input => { this.textInput = input }}
+                            placeholder="Type here..."
+                            value={text}
+                            onChangeText={onChangeText}
+                        />
+                    </View>
+
+                    {/* "Go back" button */}
+                    <View style={styles.goBack}>
+                        <Button title="GO BACK" onPress={() => navigation.goBack()} />
+                    </View>
+
+                    {/* "Clear all" button */}
+                    <View style={styles.clearAll}>
+                        <Button title="CLEAR ALL" onPress={() => this.textInput.clear()}/>
+                    </View>
+        
+                </View>
+            </TouchableWithoutFeedback>
+        </KeyboardAvoidingView>
+
     );
 }
 
-export default Screen4;
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+    textEntry: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    goBack: {
+        flexDirection: 'column',
+        justifyContent: 'flex-start',
+        position: 'absolute',
+        top: 50,
+        left: 15
+    },
+    clearAll: {
+        flexDirection: 'column',
+        justifyContent: 'flex-start',
+        position: 'absolute',
+        top: 50,
+        right: 15
+    }
+});
+
+export default TypeAnything;
