fc8b8dab22dae3de1d6361c67fb1d3cc07d62b40
Cullen Ye <59585764+CullenLYe@users.noreply.github.com>
2023-10-17 22:10:40 -0400

    Started working on Summary UI

--- src/screens/test.tsx
+++ src/screens/test.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import { RootState } from '../redux/store';
-import { Text, SafeAreaView, StyleSheet, ScrollView } from 'react-native';
+import { Text, SafeAreaView, StyleSheet, ScrollView, View } from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
 
 
 const DisplayStateComponent: React.FC = () => {
@@ -17,60 +18,70 @@ const DisplayStateComponent: React.FC = () => {
     const mentalState = useSelector((state: RootState) => state.mentalSlice);
 
     return (
-        <SafeAreaView style={styles.container}>
-            <ScrollView>
-                <Text style={styles.title}>Summary</Text>
-                <Text style={styles.subtitle}>Text Entry State</Text>
-                <Text style={styles.content}>{JSON.stringify(textEntryState, null, 2)}</Text>
+        <LinearGradient colors={["#040306", "#131624"]} style={{ flex: 1 }}>
+
+            <SafeAreaView style={styles.container}>
+                <ScrollView>
+                    <View style={{ padding: 60 }}>
+                        <Text style={styles.title}>Summary of the Information:</Text>
+                        <Text style={styles.subtitle}>Text Tab:</Text>
+                        <Text style={styles.content}>{JSON.stringify(textEntryState, null, 2)}</Text>
+
+                        <Text style={styles.subtitle}>Personal Information:</Text>
+                        <Text style={styles.content}>{JSON.stringify(personalState, null, 2)}</Text> 
+
+                        <Text style={styles.subtitle}>Allergies:</Text>
+                        <Text style={styles.content}>{JSON.stringify(allergiesState, null, 2)}</Text>
+
+                        <Text style={styles.subtitle}>Medical Condition:</Text>
+                        <Text style={styles.content}>{JSON.stringify(conditionState, null, 2)}</Text>
+
+                        <Text style={styles.subtitle}>Drugs & Medications</Text>
+                        <Text style={styles.content}>{JSON.stringify(drugState, null, 2)}</Text>
+
+                        <Text style={styles.subtitle}>Pain:</Text>
+                        <Text style={styles.content}>{JSON.stringify(painState, null, 2)}</Text> 
+
+                        <Text style={styles.subtitle}>Mental Health:</Text>
+                        <Text style={styles.content}>{JSON.stringify(mentalState, null, 2)}</Text>
+
+                        <Text style={styles.subtitle}>Alert & Oriented Assessment:</Text>
+                        <Text style={styles.content}>{JSON.stringify(assessmentState, null, 2)}</Text> 
                 
-                <Text style={styles.subtitle}>Input6 State</Text>
-                <Text style={styles.content}>{JSON.stringify(input6State, null, 2)}</Text>
-
-                <Text style={styles.subtitle}>Pain State</Text>
-                <Text style={styles.content}>{JSON.stringify(painState, null, 2)}</Text> 
-
-                <Text style={styles.subtitle}>Assessment State</Text>
-                <Text style={styles.content}>{JSON.stringify(assessmentState, null, 2)}</Text> 
-
-                <Text style={styles.subtitle}>Personal State</Text>
-                <Text style={styles.content}>{JSON.stringify(personalState, null, 2)}</Text> 
-
-                <Text style={styles.subtitle}>Allergies State</Text>
-                <Text style={styles.content}>{JSON.stringify(allergiesState, null, 2)}</Text>
-
-                <Text style={styles.subtitle}>Drug State</Text>
-                <Text style={styles.content}>{JSON.stringify(drugState, null, 2)}</Text>
-
-                <Text style={styles.subtitle}>Condition State</Text>
-                <Text style={styles.content}>{JSON.stringify(conditionState, null, 2)}</Text>
-
-                <Text style={styles.subtitle}>Mental State</Text>
-                <Text style={styles.content}>{JSON.stringify(mentalState, null, 2)}</Text>
-                
-            </ScrollView>
-        </SafeAreaView>
+                        <Text style={styles.subtitle}>Do Not Assist:</Text>
+                        <Text style={styles.content}>{JSON.stringify(input6State, null, 2)}</Text>
+                    </View>
+                </ScrollView>
+            </SafeAreaView>
+        </LinearGradient>
     );
 }
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        padding: 10,
+        marginTop: -50,
     },
     title: {
+        color: 'white',
         fontSize: 20,
         fontWeight: 'bold',
         marginBottom: 10,
+        textAlign: 'center',
     },
     subtitle: {
+        color: 'white',
         fontSize: 18,
         fontWeight: '600',
         marginTop: 10,
+        textAlign: 'center',
     },
     content: {
+        color: 'white',
         fontSize: 14,
         padding: 5,
         borderRadius: 5,
+        textAlign: 'center',
     }
 });
 
