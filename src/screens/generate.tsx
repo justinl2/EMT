22e21e4b8bda1abd4b27389f128d61d46151f999
Cullen Ye <59585764+CullenLYe@users.noreply.github.com>
2023-10-17 22:11:04 -0400

    Started working on Summary UI

--- src/screens/generate.tsx
+++ src/screens/generate.tsx
@@ -13,15 +13,8 @@ const GenerateScreen = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
     backgroundColor: '#F5FCFF',
-  },
-  text: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
+  }
 });
 
 export default GenerateScreen;
